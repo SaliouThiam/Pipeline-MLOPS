746a742d8b8eb558a7673cca0bf3e94603170ce0
ssaliou <tsaliou728@gmail.com>
2025-07-28 21:13:18 +0000

    modification du fichier test

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,3 +1,9 @@
+import sys
+import os
+
+# Ajouter le dossier parent au path pour accéder à train.py
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 from train import get_model, get_test_data, get_accuracy
 from sklearn.metrics import accuracy_score
 
@@ -5,6 +11,4 @@ def test_model_accuracy():
     clf = get_model()
     X_test, y_test = get_test_data()
     acc = accuracy_score(y_test, clf.predict(X_test))
-    
-    # Ou simplement : acc = get_accuracy()
     assert acc > 0.7
