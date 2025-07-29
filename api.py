a12e56a82b94bea48a4cb8c4aafe1f54b3dae56f
ssaliou <tsaliou728@gmail.com>
2025-07-29 20:42:18 +0000

    ajouter un port

--- api.py
+++ api.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
 import joblib
 import numpy as np
+import uvicorn
+import os
 
 app = FastAPI()
 model = joblib.load("model.joblib")
@@ -13,3 +15,7 @@ def root():
 def predict(features: list):
     prediction = model.predict([features])
     return {"prediction": int(prediction[0])}
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))  # Render fournit le port via variable d'env
+    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=False)
