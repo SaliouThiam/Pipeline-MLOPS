b2657d3936eade6c06edd85b4ac93e25972b3630
ssaliou <tsaliou728@gmail.com>
2025-07-29 21:33:45 +0000

    modification sur les données entrée

--- api.py
+++ api.py
@@ -1,8 +1,13 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import joblib
 import numpy as np
 import uvicorn
 import os
+
+# Définir un modèle pour les données d'entrée
+class FeaturesInput(BaseModel):
+    features: list[float]
 
 app = FastAPI()
 model = joblib.load("model.joblib")
@@ -12,10 +17,10 @@ def root():
     return {"message": "Modèle prêt à faire des prédictions."}
 
 @app.post("/predict")
-def predict(features: list):
-    prediction = model.predict([features])
+def predict(data: FeaturesInput):
+    prediction = model.predict([data.features])
     return {"prediction": int(prediction[0])}
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Render fournit le port via variable d'env
-    uvicorn.run("api:app", host="0.0.0.0", port=port, reload=False)
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("api:app", host="0.0.0.0", port=port)
